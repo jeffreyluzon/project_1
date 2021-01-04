6caaaeac7fcad1523ebff5a2d6ff541db7bffb19
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-01-04 17:40:54 -0500

    added boundaries for player and zombies

--- script.js
+++ script.js
@@ -5,25 +5,42 @@
 const game = document.getElementById('canvas')
 let start = document.getElementById('start')
 let ctx = game.getContext('2d')
+let click = false
 let hero;
 let zombie;
+let zombieInterval;
 // let bullet;
 let gameOver = true;
 let zombies = []
 let bullets = []
 
+function pushZombies() {
+    zombies.push(new Crawler (770, Math.floor(Math.random() * (600-70)),'blue', 30, 70))
+
+}
+//zombie shit
 function addZombies() {
     zombies.forEach((zombie, i) => {
         //add collision detection
         zombieMoves(zombie)
         zombie.render()
+        
+        // zombies.push(new Crawler (770, Math.floor(Math.random() * 600),'blue', 30, 70))
+
     })
 //  console.log('show zombie')
 }
-// function zombieInterval () {
-//     setTimeout(addZombies(), 3000);
-// }
-
+// make ZOMBIE move forward
+function zombieMoves(zombie) {
+    if (zombie.x > 0) {
+        zombie.x -= 1;
+    } else {
+        console.log('gameover')
+        gameOver = true
+        zombie.clearRect
+    }
+}
+// zombie end
 
 function Crawler (x, y, color, width, height) {
     this.x = x
@@ -74,16 +91,7 @@ function gameLoop () {
     } 
 
 }
-// make ZOMBIE move forward
-function zombieMoves(zombie) {
-    if (zombie.x > 0) {
-        zombie.x -= 1;
-    } else {
-        console.log('gameover')
-        gameOver = true
-        zombie.clearRect
-    }
-}
+
 // key button mapping for moving
 function keyDownHandler(e) {
     e.preventDefault()
@@ -92,10 +100,6 @@ function keyDownHandler(e) {
         // console.log('W pressed')
         hero.y -= 10
     }
-        if (hero.y === 600) {
-            hero.y -= 0
-            console.log('stop')
-        }
     if (e.code === 'KeyA') {
         // console.log('A pressed')
         hero.x -= 10
@@ -112,29 +116,47 @@ function keyDownHandler(e) {
         // console.log('boom')
         // this adds 1 bullet to the bullets array
         bullets.push(new Crawler (hero.x, hero.y, 'purple', 10, 10))
-    
+
+    }
+    if (e.code === 'KeyE') {
+        zombies.push(new Crawler (770, Math.floor(Math.random() * 530),'blue', 30, 70))
+        console.log('E pressed')
+        console.log(zombies)
+    } 
+    if (hero.x > game.width - hero.width) {
+        hero.x = game.width - hero.width
+    }
+    if (hero.x < 0) {
+        hero.x = 0
+    }
+    if (hero.y > game.height - hero.height) {
+        hero.y = game.height - hero.height
+    }
+    if (hero.y < 0) {
+        hero.y = 0
     }
 }
-
+function bulletHit() {
+    if(bullets.x + bullets.width > zombies.x && bullets.x < zombie.width + zombie.x)
+}
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
     console.log('dom content loaded')  
     hero = new Crawler (50, 270 ,'red', 30, 70)
     // bullets = new Crawler (hero.x, hero.y, 'purple', 10, 10)
-    zombies.push(new Crawler (770, Math.floor(Math.random() * 600),'blue', 30, 70))
+    // zombies.push(new Crawler (770, Math.floor(Math.random() * 600),'blue', 30, 70))
     // zombie = new Crawler (770, Math.floor(Math.random() * 600),'blue', 30, 70)
   
-    
-    
+    // setInterval(addZombies, 3000)
+    // this starts the game
     start.addEventListener('click', function (){
         gameOver = false
         gameLoop()
-        
+        zombieInterval = setInterval(pushZombies, 3000);
     })
     document.addEventListener('keydown', keyDownHandler)
-    
+    console.log(zombies)
     // heroMovement()
     // drawBullet()
-    
 })
